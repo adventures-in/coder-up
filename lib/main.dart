b7ed7821e4ba84da324c6d47f94f68d5661bab1d
Nick Meinhold <nick.meinhold@gmail.com>
2021-07-24 18:50:16 +1000

    Add the analyzer

--- lib/main.dart
+++ lib/main.dart
@@ -1,68 +1,113 @@
-import 'package:flutter/material.dart';
+import 'dart:io';
 
-void main() {
-  runApp(const MyApp());
-}
+import 'package:analyzer/dart/analysis/analysis_context_collection.dart';
+import 'package:analyzer/dart/analysis/results.dart';
+import 'package:analyzer/error/error.dart';
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: const MyHomePage(),
-    );
+/// A simple example of using the AnalysisContextCollection API.
+void main(List<String> args) async {
+  FileSystemEntity entity = Directory.current;
+  if (args.isNotEmpty) {
+    String arg = args.first;
+    entity = FileSystemEntity.isDirectorySync(arg) ? Directory(arg) : File(arg);
   }
-}
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key}) : super(key: key);
+  var issueCount = 0;
+  final collection = AnalysisContextCollection(
+      includedPaths: [entity.absolute.path],
+      resourceProvider: );
 
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
+  // Often one context is returned, but depending on the project structure we
+  // can see multiple contexts.
+  for (final context in collection.contexts) {
+    print('Analyzing ${context.contextRoot.root.path} ...');
 
-class _MyHomePageState extends State<MyHomePage> {
-  final List<String> _words = [];
+    for (final filePath in context.contextRoot.analyzedFiles()) {
+      if (!filePath.endsWith('.dart')) {
+        continue;
+      }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Coder Up'),
-      ),
-      body: Center(
-        child: Column(
-          children: [
-            Expanded(
-              child: Center(
-                child: Text.rich(
-                  TextSpan(
-                    children: [
-                      const TextSpan(text: 'Hello '),
-                      const TextSpan(
-                        text: 'bold',
-                        style: TextStyle(fontWeight: FontWeight.bold),
-                      ),
-                      const TextSpan(text: ' world!'),
-                      for (var word in _words) TextSpan(text: ' $word'),
-                    ],
-                  ),
-                ),
-              ),
-            ),
-            TextField(
-              decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  hintText: 'Enter a search term'),
-              onSubmitted: (value) => setState(() {
-                _words.add(value);
-              }),
-            )
-          ],
-        ),
-      ),
-    );
+      final errorsResult = await context.currentSession.getErrors2(filePath);
+      if (errorsResult is ErrorsResult) {
+        for (final error in errorsResult.errors) {
+          if (error.errorCode.type != ErrorType.TODO) {
+            print(
+                '  \u001b[1m${error.source.shortName}\u001b[0m ${error.message}');
+            issueCount++;
+          }
+        }
+      }
+    }
   }
+
+  print('$issueCount issues found.');
 }
+
+// import 'package:flutter/material.dart';
+
+// void main() {
+//   runApp(const MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({Key? key}) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       theme: ThemeData(primarySwatch: Colors.blue),
+//       home: const MyHomePage(),
+//     );
+//   }
+// }
+
+// class MyHomePage extends StatefulWidget {
+//   const MyHomePage({Key? key}) : super(key: key);
+
+//   @override
+//   State<MyHomePage> createState() => _MyHomePageState();
+// }
+
+// class _MyHomePageState extends State<MyHomePage> {
+//   final List<String> _words = [];
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text('Coder Up'),
+//       ),
+//       body: Center(
+//         child: Column(
+//           children: [
+//             Expanded(
+//               child: Center(
+//                 child: Text.rich(
+//                   TextSpan(
+//                     children: [
+//                       const TextSpan(text: 'Hello '),
+//                       const TextSpan(
+//                         text: 'bold',
+//                         style: TextStyle(fontWeight: FontWeight.bold),
+//                       ),
+//                       const TextSpan(text: ' world!'),
+//                       for (var word in _words) TextSpan(text: ' $word'),
+//                     ],
+//                   ),
+//                 ),
+//               ),
+//             ),
+//             TextField(
+//               decoration: const InputDecoration(
+//                   border: OutlineInputBorder(),
+//                   hintText: 'Enter a search term'),
+//               onSubmitted: (value) => setState(() {
+//                 _words.add(value);
+//               }),
+//             )
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
