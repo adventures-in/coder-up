ed717a45b042a86a168e523fb3af7680bdf52e7d
Nick Meinhold <nick.meinhold@gmail.com>
2021-07-24 22:14:36 +1000

    Save file to ApplicationDocumentsDirectory
    - saved a file in ApplicationDocumentsDirectory for analysis

--- lib/main.dart
+++ lib/main.dart
@@ -3,19 +3,21 @@ import 'dart:io';
 import 'package:analyzer/dart/analysis/analysis_context_collection.dart';
 import 'package:analyzer/dart/analysis/results.dart';
 import 'package:analyzer/error/error.dart';
+import 'package:analyzer/file_system/physical_file_system.dart';
+import 'package:path_provider/path_provider.dart';
 
 /// A simple example of using the AnalysisContextCollection API.
 void main(List<String> args) async {
-  FileSystemEntity entity = Directory.current;
-  if (args.isNotEmpty) {
-    String arg = args.first;
-    entity = FileSystemEntity.isDirectorySync(arg) ? Directory(arg) : File(arg);
-  }
+  final directory = await getApplicationDocumentsDirectory();
+
+  // Create a file in the Documents folder
+  final file = File('${directory.path}/main.dart');
+  file.writeAsStringSync(codeString);
 
   var issueCount = 0;
   final collection = AnalysisContextCollection(
-      includedPaths: [entity.absolute.path],
-      resourceProvider: );
+      includedPaths: [directory.absolute.path],
+      resourceProvider: PhysicalResourceProvider.INSTANCE);
 
   // Often one context is returned, but depending on the project structure we
   // can see multiple contexts.
@@ -43,6 +45,53 @@ void main(List<String> args) async {
   print('$issueCount issues found.');
 }
 
+const codeString = r'''
+import 'dart:io';
+
+import 'package:analyzer/dart/analysis/analysis_context_collection.dart';
+import 'package:analyzer/dart/analysis/results.dart';
+import 'package:analyzer/error/error.dart';
+import 'package:analyzer/file_system/physical_file_system.dart';
+
+/// A simple example of using the AnalysisContextCollection API.
+void main(List<String> args) async {
+  FileSystemEntity entity = Directory.current;
+  if (args.isNotEmpty) {
+    String arg = args.first;
+    entity = FileSystemEntity.isDirectorySync(arg) ? Directory(arg) : File(arg);
+  }
+
+  var issueCount = 0;
+  final collection = AnalysisContextCollection(
+      includedPaths: [entity.absolute.path],
+      resourceProvider: PhysicalResourceProvider.INSTANCE);
+
+  // Often one context is returned, but depending on the project structure we
+  // can see multiple contexts.
+  for (final context in collection.contexts) {
+    print('Analyzing ${context.contextRoot.root.path} ...');
+
+    for (final filePath in context.contextRoot.analyzedFiles()) {
+      if (!filePath.endsWith('.dart')) {
+        continue;
+      }
+
+      final errorsResult = await context.currentSession.getErrors(filePath);
+      if (errorsResult is ErrorsResult) {
+        for (final error in errorsResult.errors) {
+          if (error.errorCode.type != ErrorType.TODO) {
+            print(
+                '  \u001b[1m${error.source.shortName}\u001b[0m ${error.message}');
+            issueCount++;
+          }
+        }
+      }
+    }
+  }
+
+  print('$issueCount issues found.');
+}
+''';
 // import 'package:flutter/material.dart';
 
 // void main() {
